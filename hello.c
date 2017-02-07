8db645a931f38773ad41ee90314d96fd42392200
Mitchell-Childerhose <mitchchd@gmail.com>
2017-02-06 20:36:05 -0500

    Lab 2 first commit

--- hello.c
+++ hello.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main(){
-	puts("Hello TA");
+	puts("Hello World");
 	return 0;
 }
---
+++ myShell/README.md
@@ -0,0 +1,5 @@
+#csci-3020U
+
+Group Members: 
+Kevin Lovell (100559665)
+Mitchell Childerhose (100553756)
